de7a2a642fa1e945ef7e06074a57816ccdef4565
Your Name <you@example.com>
2020-10-08 16:44:19 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+	  app     = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) =>{
+	res.render("home");
+});
+
+app.get("/about", (req, res) =>{
+	res.render("about");
+});
+
+app.listen(3000, function(){
+	console.log("Deploy Demo Has Started");
+});
